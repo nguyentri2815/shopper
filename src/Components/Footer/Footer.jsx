b15847d04f1b64f0b24c818c06bf20e2e6f1a475
Tri Nguyen <tri.nguyen@alpaca.vn>
2024-04-27 19:29:35 +0700

    Frontend/2: Css component footer,header + Routes,Link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import "./Footer.css";
+import logo from "../Assets/logo_big.png";
+import instagram_icon from "../Assets/instagram_icon.png";
+import pintester_icon from "../Assets/pintester_icon.png";
+import whatsapp_icon from "../Assets/whatsapp_icon.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-logo">
+        <Link to='/'><img src={logo} alt="logo" /></Link>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social">
+        <div className="footer-social-icons">
+          <img src={instagram_icon} alt="instagram icon" />
+          <img src={pintester_icon} alt="pintester icon" />
+          <img src={whatsapp_icon} alt="whatsapp icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
