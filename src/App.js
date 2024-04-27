918cb7690fab603ca4db7c99b0a8c535c8dddbf5
Tri Nguyen <tri.nguyen@alpaca.vn>
2024-04-27 17:34:17 +0700

    Frontend 1 : init dự án + router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
+import Product from './Pages/Product';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/shopecategory' element={<ShopCategory />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
